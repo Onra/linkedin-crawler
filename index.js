648a9266b4deb9d2648dfa91c7b2a157de336948
Onra <arnaud.christ@gmail.com>
2015-07-12 19:31:38 +0200

    Get profile's positions

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var app = express();
 var options = {
     host: 'www.linkedin.com',
     port: 443,
-    path: '',
     method: 'GET'
 }
 
@@ -22,11 +21,13 @@ app.get('/profile/:username', function(req, result) {
     https.get(options, function(res) {
         console.log('Status code: %s', res.statusCode);
 
+        res.setEncoding('utf8');
+
         var html;
 
         res.on('data', function(data) {
 
-            html += data.toString();
+            html += data;
 
         });
 
@@ -37,8 +38,6 @@ app.get('/profile/:username', function(req, result) {
 
             $('#languages .section-item').each(function(index, languageElement) {
 
-                console.log($(languageElement).find('span').html());
-
                 var language = {
                     name: $(languageElement).find('span').html(),
                     proficiency: $(languageElement).find('.languages-proficiency').html()
@@ -46,11 +45,43 @@ app.get('/profile/:username', function(req, result) {
                 languages.push(language);
             });
 
+            var positions = [];
+
+            $('#background-experience .section-item').each(function(index, positionElement) {
+
+                var dateInterval = $(positionElement).find('.experience-date-locale').html();
+
+                var endDate = $(positionElement).find('.experience-date-locale > time:nth-child(2)').html();
+                var startDate = $(positionElement).find('.experience-date-locale > time').remove().html();
+
+                if (!endDate) {
+                    $(positionElement).find('.experience-date-locale > time').remove();
+                    $(positionElement).find('.experience-date-locale > span').remove();
+                    var subDate = $(positionElement).find('.experience-date-locale').html();
+                    subDate = subDate.substring(subDate.indexOf("&#x2013;") + 9, subDate.length);
+                    subDate = subDate.substring(0, subDate.indexOf(' '));
+                    endDate = subDate;
+                }
+
+                var company = $(positionElement).find('header > h5:nth-child(3) > a').html() ?
+                                $(positionElement).find('header > h5:nth-child(3) > a').html() :
+                                $(positionElement).find('header > h5 > span').html();
+
+                var position = {
+                    title: $(positionElement).find('h4 > a').html(),
+                    startDate: startDate,
+                    endDate: endDate,
+                    company: company
+                };
+                positions.push(position);
+            });
+
             var jsonResult = {
                 userData: {
                     name: $('span.full-name').html(),
                     headline: $('.title').html(),
-                    languages: languages
+                    languages: languages,
+                    positions: positions
                 }
             };
 
